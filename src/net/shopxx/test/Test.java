faecf4569518b1fefd5924d5b67b563ca953a341
zsj00877 <zsj00877@ly.com>
2015-12-10 10:16:04 +0800

    delete infomation about personer

--- src/net/shopxx/test/Test.java
+++ src/net/shopxx/test/Test.java
@@ -1,20 +1,7 @@
-/**
- * Creation Date:2015年12月10日-上午10:00:55
- * 
- * Copyright 2008-2015 © 同程网 Inc. All Rights Reserved
- */
+
 package net.shopxx.test;
 
-/**
- * Description Of The Class<br/>
- * QQ:1211956756 
- *
- * @author 	(宗寿金 07814)
- * @version 1.0.0, 2015年12月10日-上午10:00:55
- * @since 2015年12月10日-上午10:00:55
- */
 public class Test {
-
 	public static void main(String[] args) {
 		System.out.println("this is test main method");
 	}
